53a630155eeb252517b65ecf4267b9cfe1ad9e03
codingloverzz <1049005260@qq.com>
2023-02-16 19:53:10 +0800

    update index

--- src/App.js
+++ src/App.js
@@ -1,87 +1,92 @@
 import { useEffect, useState } from "react";
 import { Route, Routes, useNavigate, useRoutes } from "react-router";
-import routes from './routers/index'
-import AppStyle from './App.module.less'
-import imgc from './pictures/topbar.png'
+import routes from "./routers/index";
+import AppStyle from "./App.module.less";
+import imgc from "./pictures/topbar.png";
 import { Button, Input } from "antd";
-import { SearchOutlined } from '@ant-design/icons'
+import { SearchOutlined } from "@ant-design/icons";
 
 function App() {
-  const [currentIndex, setCurrentIndex] = useState(1)
+  const [CurrentIndex, setCurrentIndex] = useState(1);
 
-
-  const naviget = useNavigate()
+  const naviget = useNavigate();
   const handleRoute = (path, index) => {
-    naviget(path)
-    setCurrentIndex(index)
-  }
+    naviget(path);
+    setCurrentIndex(index);
+  };
 
   function getRoutes(routes) {
-    return routes.map(item => {
+    return routes.map((item) => {
       return (
         <Route path={item.path} element={item.element} key={item.path}>
-          {
-            item.children && item.children.length && getRoutes(item.children)
-          }
+          {item.children && item.children.length && getRoutes(item.children)}
         </Route>
-      )
-    })
+      );
+    });
   }
   return (
     <div className={AppStyle.app}>
       <div className={AppStyle.appHeader}>
         <div className={AppStyle.content}>
-          <div className={AppStyle.icon}>
-          </div>
+          <div className={AppStyle.icon}></div>
           <div className={AppStyle.nav}>
-            {
-              routes.map((item, index) => {
-                if (item.notShow) {
-                  return null
-                }
-                return <div className={AppStyle.routeItem}
-                key={item.path}
-                  onClick={e => handleRoute(item.path, index)}
-                  style={currentIndex == index ?
-                    {
-                      backgroundColor: '#000000',
-                      backgroundImage: `url(${imgc})`,
-                      backgroundRepeat: 'no-repeat',
-                      backgroundPositionX: '-256px',
-                      backgroundPositionY: '-123px',
-                      backgroundSize: '320px',
-                    } :
-                    {}}
-                ><span style={currentIndex == index ? { color: '#ffffff' } : {}}>{item.title}</span></div>
-              })
-            }
-            <a href="#" className={AppStyle.routeItem}>商城</a>
-            <a href="#" className={AppStyle.routeItem}>音乐人</a>
-            <a href="#" className={AppStyle.routeItem}>下载客户端</a>
+            {routes.map((item, index) => {
+              if (item.notShow) {
+                return null;
+              }
+              return (
+                <div
+                  className={AppStyle.routeItem}
+                  key={item.path}
+                  onClick={(e) => handleRoute(item.path, index)}
+                  style={
+                    CurrentIndex == index
+                      ? {
+                          backgroundColor: "#000000",
+                          backgroundImage: `url(${imgc})`,
+                          backgroundRepeat: "no-repeat",
+                          backgroundPositionX: "-256px",
+                          backgroundPositionY: "-123px",
+                          backgroundSize: "320px",
+                        }
+                      : {}
+                  }
+                >
+                  <span
+                    style={CurrentIndex == index ? { color: "#ffffff" } : {}}
+                  >
+                    {item.title}
+                  </span>
+                </div>
+              );
+            })}
+            <a href="#" className={AppStyle.routeItem}>
+              商城
+            </a>
+            <a href="#" className={AppStyle.routeItem}>
+              音乐人
+            </a>
+            <a href="#" className={AppStyle.routeItem}>
+              下载客户端
+            </a>
           </div>
           <div className={AppStyle.search}>
-            <Input placeholder="音乐/用户/歌手"
-              prefix={<SearchOutlined />} size='small'
-              style={{ borderRadius: '20px', width: '150px', fontSize: '14px' }} />
+            <Input
+              placeholder="音乐/用户/歌手"
+              prefix={<SearchOutlined />}
+              size="small"
+              style={{ borderRadius: "20px", width: "150px", fontSize: "14px" }}
+            />
           </div>
-          <div style={{ lineHeight: '70px' }}>
+          <div style={{ lineHeight: "70px" }}>
             <Button className={AppStyle.btn}>创作者中心</Button>
           </div>
           <div className={AppStyle.login}>
-            <a href="#">
-              登录
-            </a>
+            <a href="#">登录</a>
           </div>
         </div>
-
       </div>
-      <Routes>
-        {
-          getRoutes(routes)
-        }
-      </Routes>
-
-
+      <Routes>{getRoutes(routes)}</Routes>
     </div>
   );
 }
